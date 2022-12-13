40d574f159462dc4b8a7dcca9982fc7c8b837b3d
Paweł Łęczycki <pawel@msd.info.pl>
2022-12-13 12:11:52 +0100

    Singleto fix

--- WPC/DesignPatterns/Creational/Singleton/Client.cs
+++ WPC/DesignPatterns/Creational/Singleton/Client.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using WPC.DesignPatterns.Creational.Singleton;
 
 namespace WPC.DesignPatterns.Stuctural.Bridge
 {
@@ -11,6 +12,21 @@ namespace WPC.DesignPatterns.Stuctural.Bridge
     {
         public static void Execute()
         {
+            {
+                var service = Context.Instance;
+                Console.WriteLine($"Service: {service}");
+            }
+
+            {
+                var service = Context.Instance;
+                service.SetSettings("2", "ala ma kota");
+                Console.WriteLine($"Service: {service}");
+            }
+
+            {
+                var service = Context.Instance;
+                Console.WriteLine($"Service: {service}");
+            }
         }
     }
 }
